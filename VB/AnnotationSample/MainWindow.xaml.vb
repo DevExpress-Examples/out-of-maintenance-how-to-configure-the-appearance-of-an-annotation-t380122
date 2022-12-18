9cf6bf50ff1c446b1796af5652c797ed75ec2438
DevExpressExampleBot <fakeEmail>
2022-12-18 16:17:58 +0400

    Source auto update [skip ci]

--- VB/AnnotationSample/MainWindow.xaml.vb
+++ VB/AnnotationSample/MainWindow.xaml.vb
@@ -1,32 +1,24 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
 Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace AnnotationSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Public Class TextAndImage
-		Public Property Text() As String
-		Public Property ImageSource() As ImageSource
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+
+    Public Class TextAndImage
+
+        Public Property Text As String
+
+        Public Property ImageSource As ImageSource
+    End Class
 End Namespace
